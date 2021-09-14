4946e67f2171b096afd89818fc547a720bd48ca7
okriiza <rendiokrizaputra@gmail.com>
2021-09-14 16:02:58 +0700

    install and configuration tailwindCSS

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-9xl bg-gray-700">TEsting</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
